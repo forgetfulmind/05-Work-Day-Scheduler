4fb0661c1d8f12773ad7c76cbd8d9fd589ff4532
Bryan Shay <forgetfulmind@gmail.com>
2020-12-22 14:51:39 -0500

    fixed text area, broke dynamic color update

--- logic.js
+++ logic.js
@@ -22,25 +22,17 @@ setInterval(function(){
     currentDay.append(dayjs().format('dddd MMMM DD' + " - " +'h:mm:ss:a'));
 },1000)
 
-//update every 10 mins
-setInterval(function(){
-    buildBlocks(workHours);
-                    },1000 * 60)
+// //update every 10 mins
+// setInterval(function(){
+//     buildBlocks(workHours);
+//     setButtons(workHours);
+//     saveItem();
+//                     },1000)
 
 //when I load the page I want to see 12 blocks, 1 for each hour 
 buildBlocks(workHours);
 setButtons(workHours);
 
-
-input9.value = localStorage.getItem("input9")
-input10.value = localStorage.getItem("input10")
-input11.value = localStorage.getItem("input11")
-input12.value = localStorage.getItem("input12")
-input13.value = localStorage.getItem("input13")
-input14.value = localStorage.getItem("input14")
-input15.value = localStorage.getItem("input15")
-input16.value = localStorage.getItem("input16")
-input17.value = localStorage.getItem("input17")
 
 //button functionality loop
 function setButtons (squid){
@@ -63,6 +55,7 @@ function buildBlocks(squid){
         let row = $("<div>")
         row.attr("id", squid[i].time)
         row.attr("class", "row")
+        row.attr("key", squid[i].key)
     //each block is labeled per hour
     //make time of day display
         let span = $("<div>").text(squid[i].time)
@@ -70,7 +63,7 @@ function buildBlocks(squid){
         row.append(span);
     //each block has a text field that is editable
     //make input field
-        let task = $("<input>")
+        let task = $("<textarea>")
         task.attr("id", "input"+ squid[i].key)
         task.attr("class", "input col-8")
         row.append(task);
@@ -103,3 +96,38 @@ function buildBlocks(squid){
         blockContainer.append(row)
     }}
 
+
+
+
+
+//field and storage link 
+    input9.value = localStorage.getItem("input9")
+    input10.value = localStorage.getItem("input10")
+    input11.value = localStorage.getItem("input11")
+    input12.value = localStorage.getItem("input12")
+    input13.value = localStorage.getItem("input13")
+    input14.value = localStorage.getItem("input14")
+    input15.value = localStorage.getItem("input15")
+    input16.value = localStorage.getItem("input16")
+    input17.value = localStorage.getItem("input17")
+
+// function setColors(){
+
+
+// //class dynamic update
+//         //past hour blocks have class .past 
+//         if (parseInt($(this).key) < parseInt(dayjs().format('H'))){
+//             $(this).attr("class","row past")
+//         }
+              
+// //current hour block has class .present 
+//         else if (parseInt($(this).key) === parseInt(dayjs().format('H'))){
+//             $(this).attr("class","row present")        
+//         }
+
+// //future hour blocks have classfuture
+//         else {
+//             $(this).attr("class","row future")
+        
+//         }
+// }
